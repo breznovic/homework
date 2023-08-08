f5b5d875dd6a40098a3db90f8581d9cd63689f9b
breznovic <slymarkel@gmail.com>
2023-08-08 12:29:01 +0300

    HW8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -5,12 +5,6 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import User from './User'
 
-/*
-* 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
-* 2 - дописать компоненту User
-* 3 - сделать стили в соответствии с дизайном
-* */
-
 export type UserType = {
     _id: number
     name: string
@@ -34,24 +28,24 @@ const HW8 = () => {
     const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
 
     const sortUp = () => {
-        setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
-        ) // в алфавитном порядке a.name > b.name
-        setCurrentSort('up')
-    }
+		setPeople(
+			homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
+		); // в алфавитном порядке a.name > b.name
+		setCurrentSort('up');
+	};
 
-    const sortDown = () => {
-        setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
-        ) // в обратном порядке a.name < b.name}
-        setCurrentSort('down')
-    }
-    const check18 = () => {
-        setPeople(
-            homeWorkReducer(initialPeople, {type: 'check', payload: 18})
-        ) // совершеннолетние
-        setCurrentSort('18')
-    }
+	const sortDown = () => {
+		setPeople(
+			homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
+		); // в обратном порядке a.name < b.name}
+		setCurrentSort('down');
+	};
+	const check18 = () => {
+		setPeople(
+			homeWorkReducer(initialPeople, {type: 'check', payload: 18})
+		); // совершеннолетние
+		setCurrentSort('18');
+	};
 
     return (
         <div id={'hw3'}>
